e269b1482f6f41c39fec0093d66ecc0537cd725e
Gus Dunn <w.gus.dunn@gmail.com>
2017-10-10 19:11:21 -0400

    Bump version: 0.0.4 → 0.0.5

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ test_requirements = [
 
 setup(
     name='snaketools',
-    version='0.0.4',
+    version='0.0.5',
     description="Small library of helper tools for setting up, graphing, and working with Snakemake rules.",
     long_description=readme + '\n\n' + history,
     author="Gus Dunn",
